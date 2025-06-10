a1d0342c7c532243aa24a8d697041770ab74c2b0
06Stvo <06stvo@gmail.com>
2025-06-10 16:23:10 -0400

    Queried the order ids and comments from database

--- database.php
+++ database.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 $servername = "localhost";
 $username = "root";
@@ -13,6 +13,15 @@ if ($conn->connect_error) {
     die("Connection Failed: " . $conn->connect_error);
 }
 
-echo "Connected!";
+echo "Connected!" . "<br>";
 
-?>
+$sql = "SELECT orderid, comments FROM sweetwater_test";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "orderid: " . $row["orderid"] . " | Comment: " . $row["comments"] . "<br>";
+    }
+} else {
+    echo "No Results";
+}
