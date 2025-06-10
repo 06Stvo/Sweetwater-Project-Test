83af1045b405ed9a3d51e6eb44aa4fcbeb21bcfd
06Stvo <06stvo@gmail.com>
2025-06-10 16:12:45 -0400

    Connected to the Database

---
+++ database.php
@@ -0,0 +1,18 @@
+<?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "sweetwaterdb";
+$conn = "";
+
+$conn = mysqli_connect($servername, $username, $password, $db_name);
+
+
+if ($conn->connect_error) {
+    die("Connection Failed: " . $conn->connect_error);
+}
+
+echo "Connected!";
+
+?>
